e628a89cf30c51c9c610ddaea906d622b97cfc1d
tiziano <tizianoerosbehm@gmail.com>
2024-12-04 22:42:24 -0300

    se aplico el menu hamburguesa y se puso efectos a los hover

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function viewDemo(link){
 	const demo = document.getElementById('demo');
 	demo.setAttribute('src', link);
 	demo.style.display = "flex";
-	console.log(link);
 	window.onclick = function(event) {
 		console.log(event.target);
         if (event.target.getAttribute('class') != "project" && event.target.getAttribute('class') != "project-img" && event.target.getAttribute('class') != "tools") {
