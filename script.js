b74d75fc2119c8b0be411bee8d5a8db731920fad
tiziano <tizianoerosbehm@gmail.com>
2024-12-08 14:43:29 -0300

    fix overflow lateral y navegador

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function viewDemo(link){
         }
     } 
 }
+let clicked = false;
+hamburger.addEventListener('click', e=>{
+	if(clicked){
+		hamburger.style.height = "1em";
+		hamburger.style.background = "#060415"
+		clicked = false;
+	}
+	else{
+		hamburger.style.height = "6.5em";
+		hamburger.style.background = "#181516"
+		clicked = true;
+	}
+	
+})
